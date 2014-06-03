4988b0e750bcab1ed50138b31fcdd006b933d517
Boy Maas <boy.maas@gmail.com>
2014-06-03 04:58:04 +0200

    Better color for helm search

--- init.el
+++ init.el
@@ -71,3 +71,15 @@ FILENAME defaults to `buffer-file-name'."
 (org-babel-load-file (expand-file-name "core.org" bmaas/home-dir))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "green" :foreground "color-16")))))
