48104d10380a148d6d005b48bcafe95547167070
Boy Maas <boy.maas@gmail.com>
2015-03-02 21:23:59 +0100

    Tweaks

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-macroexpansion-suppress-namespaces nil)
  '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(evil-highlight-closing-paren-at-point-states (quote (not emacs insert replace normal)))
  '(helm-input-idle-delay 0.2)
